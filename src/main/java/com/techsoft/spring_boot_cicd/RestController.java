d1f0c00d3481c32631fea03f3a00dc363ace0fac
Santosh7338 <81172185+Santosh7338@users.noreply.github.com>
2024-11-22 21:59:55 +0530

    added

---
+++ src/main/java/com/techsoft/spring_boot_cicd/RestController.java
@@ -0,0 +1,14 @@
+package com.techsoft.spring_boot_cicd;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+	
+	@GetMapping("/welcome")
+	public String test() {
+		
+		return "welcome to docker cicd!";
+	}
+
+}
