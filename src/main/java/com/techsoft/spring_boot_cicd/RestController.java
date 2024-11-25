b83c7c84a92d165a0fb8fe6e45190eba7320b167
Santosh7338 <81172185+Santosh7338@users.noreply.github.com>
2024-11-25 12:15:06 +0530

    added

--- src/main/java/com/techsoft/spring_boot_cicd/RestController.java
+++ src/main/java/com/techsoft/spring_boot_cicd/RestController.java
@@ -8,7 +8,7 @@ public class RestController {
 	@GetMapping("/welcome")
 	public String test() {
 		
-		return "welcome to docker cicd version-3";
+		return "welcome to docker cicd version-4";
 	}
 
 }
